aca4c74b673359a5b7caf2bbbd9a7bd50bec686c
volkanikiz <56491296+volkanikiz@users.noreply.github.com>
2025-07-15 20:07:57 +0300

    Add full navigation menu to admin header

--- admin/edit_tour.php
+++ admin/edit_tour.php
@@ -26,10 +26,21 @@ $tour = $tours[$id];
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Admin</a>
-        <div class="collapse navbar-collapse">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="index.php">Dashboard</a></li>
                 <li class="nav-item"><a class="nav-link" href="tours.php">Turlar</a></li>
+                <li class="nav-item"><a class="nav-link" href="new_tour.php">Yeni Tur</a></li>
+                <li class="nav-item"><a class="nav-link" href="rota.php">Rota & Takvim</a></li>
+                <li class="nav-item"><a class="nav-link" href="basvurular.php">Başvurular</a></li>
+                <li class="nav-item"><a class="nav-link" href="yorumlar.php">Yorumlar</a></li>
+                <li class="nav-item"><a class="nav-link" href="sss.php">SSS</a></li>
+                <li class="nav-item"><a class="nav-link" href="galeri.php">Galeri</a></li>
+                <li class="nav-item"><a class="nav-link" href="ayarlar.php">Ayarlar</a></li>
+                <li class="nav-item"><a class="nav-link" href="kullanicilar.php">Kullanıcılar</a></li>
             </ul>
         </div>
     </div>
